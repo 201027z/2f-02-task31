b6629f319b59b29385fba2d67889eb0d17339543
201027z <115199197+201027z@users.noreply.github.com>
2022-10-14 09:27:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+let z = 0
+input.onButtonPressed(Button.A, function () {
+    z = -1
+})
+input.onButtonPressed(Button.B, function () {
+    z = 10
+    while (z >= 0) {
+        basic.showNumber(z)
+        z += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+})
